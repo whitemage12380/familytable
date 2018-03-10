613144098fe88e438916be05c34b860b48f2a641
Egan Neuhengen <lightningworks@gmail.com>
2018-03-10 13:58:44 -0800

    Adding migration for user's default family

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101232355) do
+ActiveRecord::Schema.define(version: 20180310215241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180101232355) do
     t.text "display_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "default_family_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
@@ -96,4 +97,5 @@ ActiveRecord::Schema.define(version: 20180101232355) do
   add_foreign_key "family_dishes", "family_dishes", column: "parent_id"
   add_foreign_key "family_members", "families"
   add_foreign_key "family_members", "users"
+  add_foreign_key "users", "families", column: "default_family_id"
 end
