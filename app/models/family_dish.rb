60edaa087a965c27cf50732fb1f2907b9e976d16
Egan Neuhengen <lightningworks@gmail.com>
2018-09-22 09:36:01 -0700

    Dishes have many ingredients

--- app/models/family_dish.rb
+++ app/models/family_dish.rb
@@ -3,4 +3,5 @@ class FamilyDish < ActiveRecord::Base
   #                :comfort_level, :health_level, :cooking_difficulty, :is_prepared_ahead, 
   #                :prep_time_minutes, :cooking_time_minutes, :serving_size, :protein_grams, :calories
   belongs_to :family
+  has_many :ingredients, through: :family_dish_ingredients
 end
